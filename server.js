a5627f6bd642ea31d3d4ec4f6742d2fb9582a371
Fernando Martinez <frndmartinezglez@gmail.com>
2019-11-24 09:23:14 -0500

    Update REST API implementation

--- server.js
+++ server.js
@@ -8,56 +8,15 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-let todos = [];
-
 app.prepare().then(() => {
   const server = express();
 
   const jsonParser = bodyParser.json();
 
-  server.get("/api/todos", (_req, res) => {
-    res.send(todos);
-  });
-
-  server.post("/api/todos", jsonParser, (req, res) => {
-    const { done, content, starred } = req.body;
-
-    const todo = { done, content, starred, id: todos.length + 1 };
-    todos = todos.concat(todo);
-
-    res.status(201).send(todo);
-  });
-
-  server.put("/api/todos/:id", jsonParser, (req, res) => {
-    const id = Number(req.params.id);
-
-    const idx = todos.findIndex(x => x.id === id);
-    if (idx < 0) {
-      return res.sendStatus(404);
-    }
-
-    const { done, content, starred } = req.body;
-
-    todos = todos.map(x =>
-      x.id === id ? { ...x, done, content, starred } : { ...x }
-    );
-
-    res.send(todos.find(x => x.id === id));
-    return;
-  });
-
-  server.delete("/api/todos/:id", (req, res) => {
-    const { id } = req.params;
-
-    const idx = todos.findIndex(x => x.id === id);
-    if (idx < 0) {
-      return res.sendStatus(404);
-    }
-
-    todos = todos.filter(x => x.id === id);
-
-    return res.send();
-  });
+  server.get("/api/todos", getAll);
+  server.post("/api/todos", jsonParser, createTODO);
+  server.put("/api/todos/:id", jsonParser, updateTODO);
+  server.delete("/api/todos/:id", deleteTODO);
 
   server.all("*", (req, res) => {
     return handle(req, res);
@@ -71,3 +30,47 @@ app.prepare().then(() => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
+
+let todos = [];
+
+const getAll = (_req, res) => {
+  res.status(200).send(todos);
+};
+
+const createTODO = (req, res) => {
+  const { done, content, starred } = req.body;
+
+  const todo = { done, content, starred, id: todos.length + 1 };
+
+  todos = todos.concat(todo);
+  res.status(201).send(todo);
+};
+
+const updateTODO = (req, res) => {
+  const id = Number(req.params.id);
+
+  const idx = todos.findIndex(x => x.id === id);
+  if (idx < 0) {
+    return res.sendStatus(404);
+  }
+
+  const { done, content, starred } = req.body;
+
+  const todo = todos[idx];
+  todos[idx] = { ...todo, done, content, starred };
+
+  return res.status(200).send(todos[idx]);
+};
+
+const deleteTODO = (req, res) => {
+  const id = Number(req.params.id);
+
+  const idx = todos.findIndex(x => x.id === id);
+  if (idx < 0) {
+    return res.sendStatus(404);
+  }
+
+  todos = todos.filter((_, i) => i !== idx);
+
+  return res.status(200).send();
+};
